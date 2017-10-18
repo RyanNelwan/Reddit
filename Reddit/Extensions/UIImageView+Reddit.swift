fe0a6a538d58d445470c8464d0e5846960d4dcee
Ryan <ryannelwan@gmail.com>
2017-10-18 16:33:52 -0700

    New UIImageView extension to allow asynchronous downloads

---
+++ Reddit/Extensions/UIImageView+Reddit.swift
@@ -0,0 +1,27 @@
+//
+//  UIImageView+Reddit.swift
+//  Reddit
+//
+//  Created by Ryan Nelwan on 10/18/17.
+//  Copyright © 2017 Ryan Nelwan. All rights reserved.
+//
+
+import UIKit
+
+
+extension UIImageView {
+    
+    // Asynchronous image download
+    public func downloadImage(with string: String) {
+        URLSession.shared.dataTask(with: NSURL(string: string)! as URL, completionHandler: { (data, response, error) -> Void in
+            if error != nil {
+                print(error!)
+                return
+            }
+            DispatchQueue.main.async(execute: { () -> Void in
+                let image = UIImage(data: data!)
+                self.image = image
+            })
+        }).resume()
+    }
+}
