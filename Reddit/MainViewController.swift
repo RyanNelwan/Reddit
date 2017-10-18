fd4d71d5fcf5df52e1ef7f4b2d61810c1590696d
Ryan <ryannelwan@gmail.com>
2017-10-18 12:17:57 -0700

    New PostTableViewCell

--- Reddit/MainViewController.swift
+++ Reddit/MainViewController.swift
@@ -13,7 +13,7 @@ class MainViewController: UIViewController {
     @IBOutlet var tableView: UITableView!
     
     // Temp Fixtures
-    let data = ["Some Data", "Another data value"]
+    let data = ["Some data", "Another data value", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu tristique urna. Praesent faucibus ligula quis turpis vehicula mattis. Curabitur odio lectus, dapibus quis auctor et, imperdiet a eros. Proin gravida sit amet sapien et accumsan. Nullam sit amet mauris nunc. Donec consectetur justo mauris, quis viverra mi sodales quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec non nibh et diam suscipit accumsan a nec lorem. Nulla id interdum libero, sed dapibus ex."]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,7 +38,7 @@ extension MainViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = self.tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "Post", for: indexPath) as! PostTableViewCell
         cell.textLabel?.text = self.data[indexPath.row]
         return cell
     }
@@ -47,4 +47,7 @@ extension MainViewController: UITableViewDelegate, UITableViewDataSource {
         return 50.0
     }
     
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150.0
+    }
 }
---
+++ Reddit/TableViewCells/PostTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PostTableViewCell.swift
+//  Reddit
+//
+//  Created by Ryan Nelwan on 10/18/17.
+//  Copyright © 2017 Ryan Nelwan. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+    
+}
