87d30399c300b5802e85b1963d22bb71e2dd46b2
Ryan <ryannelwan@gmail.com>
2017-10-18 14:35:15 -0700

    New RequestManager struct

---
+++ Reddit/Utils/RequestManager.swift
@@ -0,0 +1,15 @@
+//
+//  RequestManager.swift
+//  Reddit
+//
+//  Created by Ryan Nelwan on 10/18/17.
+//  Copyright © 2017 Ryan Nelwan. All rights reserved.
+//
+
+import Foundation
+
+struct RequestManager {
+    let baseURLString = "https://www.reddit.com"
+    let path = "/top"
+    let format = "json"
+}
