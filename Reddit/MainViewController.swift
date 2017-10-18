32b55bfe7b2a594499a2b121b5c5576b8ed9fc34
Ryan <ryannelwan@gmail.com>
2017-10-18 12:05:50 -0700

    Populate table view with fixtures

--- Reddit/MainViewController.swift
+++ Reddit/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  Reddit
 //
 //  Created by Ryan Nelwan on 10/18/17.
@@ -12,12 +12,15 @@ class MainViewController: UIViewController {
     
     @IBOutlet var tableView: UITableView!
     
+    // Temp Fixtures
+    let data = ["Some Data", "Another data value"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -27,15 +30,17 @@ class MainViewController: UIViewController {
 extension MainViewController: UITableViewDelegate, UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
+        return 1
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return self.data.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = self.data[indexPath.row]
+        return cell
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
