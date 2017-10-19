1da098eb43132500fd5c88314fb1601bfee6cbb8
Ryan <ryannelwan@gmail.com>
2017-10-18 23:38:44 -0700

    Completion and failure closures

--- Reddit/Extensions/UIImageView+Reddit.swift
+++ Reddit/Extensions/UIImageView+Reddit.swift
@@ -11,15 +11,22 @@ import UIKit
 extension UIImageView {
 
     // Asynchronous image download
-    public func downloadImage(with string: String) {
+    public func downloadImage(with string: String, completion: (() -> Void)!, failure: ((_ error: Error) -> Void)!) {
         URLSession.shared.dataTask(with: NSURL(string: string)! as URL, completionHandler: { (data, response, error) -> Void in
-            if error != nil {
-                print(error!)
-                return
-            }
             DispatchQueue.main.async(execute: { () -> Void in
+                if error != nil {
+                    if failure != nil {
+                        failure(error!)
+                    }
+                    return
+                }
+                
                 let image = UIImage(data: data!)
                 self.image = image
+                
+                if completion != nil {
+                    completion()
+                }
             })
         }).resume()
     }
